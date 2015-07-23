986f3b880ad9c71e9460c41e62497b1909890c8e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-07-23 12:37:09 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 var sh = require('shelljs');
 var parseDuration = require('duration-parser');
+var moment = require('moment');
 var gh = module.exports = {};
 
-var hoursPattern = /\[(((\d+)[h|m])+)\]/;
+var brackets = /\[(.+)\]/;
 
-gh.exec = function(options, cb) {
-  var gitCmd = 'git log';
+gh.exec = function(opts, cb) {
+  var args = [];
+  if (opts.since) {
+    args.push('--since');
+    args.push('"'+moment(new Date(opts.since)).format()+'"');
+  }
+  if (opts.until) {
+    args.push('--until');
+    args.push('"'+moment(new Date(opts.until)).format()+'"');
+  }
+  var gitCmd = 'git log '+args.join(' ');
   sh.exec(gitCmd, { silent: true }, function(code, data) {
     if (code !== 0) cb(new Error('git exited non-zero'));
     var items = [];
     data.split(/commit/g).forEach(function(commit) {
-      var matchHours = commit.match(hoursPattern);
+      var matchHours = commit.match(brackets);
       if (matchHours) {
         var item = {}
-        item.durationInput = matchHours[1];
+        item.input = matchHours[1];
         item.duration = parseDuration(matchHours[1]);
         commitParts = commit.trim().split('\n');
         item.sha = commitParts[0];
         item.author = commitParts[1].match(/Author:\s+(.+)/)[1];
         var msg = commitParts.slice(3, commitParts.length);
-        item.message = msg.join('\n').replace(hoursPattern, '').trim();
+        item.message = msg.join('\n').replace(brackets, '').trim();
         item.date = new Date(commitParts[2].match(/Date:\s+(.+)/)[1]);
         items.push(item);
       }
